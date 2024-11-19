483a0550d70388338c605f5dcaa8590119f544ee
max-oyedele <eastsea1020n@gmail.com>
2024-11-19 07:40:39 -0500

    add 1567

---
+++ 726~/1567.maximum-length-of-subarray-with-positive-product.ts
@@ -0,0 +1,21 @@
+function getMaxLen(nums: number[]): number {
+    let n1 = nums[0] > 0 ? 1 : 0,
+        n2 = nums[0] < 0 ? 1 : 0;
+    let ans = n1;
+    for (let i = 1; i < nums.length; ++i) {
+        let cur = nums[i];
+        if (cur == 0) {
+            (n1 = 0), (n2 = 0);
+        } else if (cur > 0) {
+            ++n1;
+            n2 = n2 > 0 ? n2 + 1 : 0;
+        } else {
+            let t1 = n1,
+                t2 = n2;
+            n1 = t2 > 0 ? t2 + 1 : 0;
+            n2 = t1 + 1;
+        }
+        ans = Math.max(ans, n1);
+    }
+    return ans;
+}
